7360de1e710dd0a1f45733ea3a93f10bceb4106a
Yam <yamprado2010@hotmail.com>
2023-05-21 16:01:12 -0300

    builded calculator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,112 @@
-import Image from 'next/image'
+"use client"
+
+import { useState } from 'react'
+import { Display } from '@/components/Display'
+import { Button } from '@/components/Button';
+
+type OperationProps = {
+  displayValue: string
+  clearDisplay: boolean
+  operation: '/' | '*' | '+' | '-' | '=' | null
+  values: number[]
+  current: number
+}
+
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
 
 export default function Home() {
+  const [calculator, onCalculator] = useState<OperationProps>(initialState)
+
+  function addDigit(digit: string) {
+    const clearDisplay = calculator.displayValue === '0' || calculator.clearDisplay
+
+    if ((digit === '.' && !clearDisplay) && calculator.displayValue.includes('.')) return;
+
+    const currentValue = clearDisplay ? '' : calculator.displayValue
+    const displayValue = currentValue + digit
+
+    if (digit !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...calculator.values]
+
+      values[calculator.current] = newValue
+
+      onCalculator((fields) => ({
+        ...fields,
+        displayValue,
+        clearDisplay: false,
+        values,
+      }))
+    }
+  }
+
+  function clearOperation() {
+    onCalculator({ ...initialState });
+  }
+
+  function setOperation(operation: '/' | '*' | '+' | '-' | '=') {
+    if (calculator.current === 0) {
+      onCalculator((fields) => ({
+        ...fields,
+        operation,
+        current: 1,
+        clearDisplay: true,
+      }))
+    } else {
+      const equals = operation === '='
+      const values = [...calculator.values]
+
+      try {
+        values[0] = 
+          eval(`${values[0]} ${calculator.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = calculator.values[0]
+      }
+  
+      values[1] = 0
+
+      onCalculator((fields) => ({
+        ...fields,
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        //clearDisplay: !equals,
+        clearDisplay: true,
+        values,
+      }))
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex bg-slate-600 min-h-screen flex-col items-center justify-center p-24">
+       <div className="flex flex-col gap-4 max-w-xl w-full p-2 bg-slate-100 rounded-lg">
+         <Display label={calculator.displayValue} />
+         <div className="grid grid-cols-4 gap-4 w-full px-4 py-2">
+          <Button label='AC' className='col-span-3' role="triple" onClick={clearOperation} />
+          <Button label='/' role="operation" onClick={() => setOperation('/')} />
+          <Button label='7' onClick={() => addDigit('7')} />
+          <Button label='8' onClick={() => addDigit('8')} />
+          <Button label='9' onClick={() => addDigit('9')} />
+          <Button label='*' role="operation" onClick={() => setOperation('*')}  />
+          <Button label='4' onClick={() => addDigit('4')} />
+          <Button label='5' onClick={() => addDigit('5')} />
+          <Button label='6' onClick={() => addDigit('6')} />
+          <Button label='-' role="operation" onClick={() => setOperation('-')} />
+          <Button label='1' onClick={() => addDigit('1')} />
+          <Button label='2' onClick={() => addDigit('2')} />
+          <Button label='3' onClick={() => addDigit('3')} />
+          <Button label='+' role="operation" onClick={() => setOperation('+')} />
+          <Button label='0' className='col-span-2' role="double" onClick={() => addDigit('0')}  />
+          <Button label='.' onClick={() => addDigit('.')} />
+          <Button label='=' role="operation" onClick={() => setOperation('=')} />
+         </div>
+       </div>
     </main>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,14 +5,16 @@ import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
   label: string;
-  role: 'double' | 'triple' | 'operation'
+  role?: 'double' | 'triple' | 'operation'
 }
 
-export function Button({ label, role, ...props }: ButtonProps) {
+export function Button({ label, role, className, ...props }: ButtonProps) {
   const styled = classNames(
     role === 'operation' && 'text-white bg-orange-600',
-    role !== 'operation' && 'text-black bg-slate-100 border-slate-600',
-    'flex py-5 rounded-md text-4xl text-center font-medium border'
+    role !== 'operation' && 'text-white bg-slate-600 border-slate-900',
+    !role && 'bg-slate-100 text-black border-slate-600',
+    'flex py-5 rounded-md text-4xl text-center justify-center font-medium border transition-colors hover:opacity-70',
+    className
   ) 
     
   return (
